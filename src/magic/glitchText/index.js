2358032c6b8b4bc6cdffa81ac0ede4c79b733781
yongxu <renyongxu@gmail.com>
2016-08-06 10:41:05 -0400

    added glitch text script, but not finished

---
+++ src/magic/glitchText/index.js
@@ -0,0 +1,27 @@
+require('./index.scss')
+
+export default class GlitchText {
+  constructor(parent) {
+    this.text = document.createElement('div')
+    this.text.id = 'glitchText'
+
+    this.container = document.createElement('div')
+    this.container.innerHTML = require('raw!./filter.svg')
+    this.container.appendChild(this.text)
+    this.txt = ''
+
+    this.parent = parent
+    parent.appendChild(this.container)
+  }
+  applyText(txt) {
+    this.txt = txt
+    this.text.innerHTML = this.txt;
+    this.text.setAttribute('data-text', txt)
+  }
+
+  appendText(txt) {
+    this.txt = this.txt + txt
+    this.text.innerHTML = this.txt;
+    this.text.setAttribute('data-text', txt)
+  }
+}
