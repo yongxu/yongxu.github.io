bf830178883fe5e958a3fbf337a1ffe9b7308d22
yongxu <renyongxu@gmail.com>
2016-08-19 23:58:04 -0400

    glitch text is working

--- src/magic/glitchText/index.js
+++ src/magic/glitchText/index.js
@@ -1,4 +1,8 @@
 require('./index.scss')
+import {
+  randomList,
+  nonLinearInterpellationGenerator
+} from 'utils/random'
 
 export default class GlitchText {
   constructor(parent) {
@@ -12,6 +16,62 @@ export default class GlitchText {
 
     this.parent = parent
     parent.appendChild(this.container)
+
+    this.rOffset = document.getElementById('r-offset')
+    this.gOffset = document.getElementById('g-offset')
+    this.bOffset = document.getElementById('b-offset')
+
+    this.motionStarted = false
+}
+
+  dr = (x, y) => {
+    this.rOffset.setAttributeNS(null, "dx", x);
+    this.rOffset.setAttributeNS(null, "dy", y);
+  }
+
+  dg = (x, y) => {
+    this.gOffset.setAttributeNS(null, "dx", x);
+    this.gOffset.setAttributeNS(null, "dy", y);
+  }
+
+  db = (x, y) => {
+    this.bOffset.setAttributeNS(null, "dx", x);
+    this.bOffset.setAttributeNS(null, "dy", y);
+  }
+
+  startMotion = (rFx, rFy, gFx, gFy, bFx, bFy) => {
+    let start = 0
+    const step = (timestamp) => {
+      if (!start) start = timestamp
+      const t_sec = (timestamp - start) / 1000.0
+      const r_dx = rFx ? rFx(t_sec) : 0
+      const r_dy = rFy ? rFy(t_sec) : 0
+      const g_dx = gFx ? gFx(t_sec) : 0
+      const g_dy = gFy ? gFy(t_sec) : 0
+      const b_dx = bFx ? bFx(t_sec) : 0
+      const b_dy = bFy ? bFy(t_sec) : 0
+      this.dr(r_dx, r_dy)
+      this.dg(g_dx, g_dy)
+      this.db(b_dx, b_dy)
+
+      if (this.motionStarted) window.requestAnimationFrame(step)
+    }
+    this.motionStarted = true
+    this.start = 0
+    window.requestAnimationFrame(step)
+  }
+
+  startRandomMotion = () => {
+    const randomListLength = 10
+    const speed = 5
+    const rxf = nonLinearInterpellationGenerator(randomList(randomListLength, -0.0, 7.0, 1), speed)
+    const ryf = nonLinearInterpellationGenerator(randomList(randomListLength, -3.0, 3.0, 1), speed)
+    const gxf = nonLinearInterpellationGenerator(randomList(randomListLength, -3.0, 3.0, 1), speed)
+    const gyf = nonLinearInterpellationGenerator(randomList(randomListLength, -3.0, 3.0, 1), speed)
+    const bxf = nonLinearInterpellationGenerator(randomList(randomListLength, -7.0, 0.0, 1), speed)
+    const byf = nonLinearInterpellationGenerator(randomList(randomListLength, -3.0, 3.0, 1), speed)
+
+    this.startMotion(rxf, ryf, gxf, gyf, bxf, byf)
   }
   applyText(txt) {
     this.txt = txt
--- src/utils/random.js
+++ src/utils/random.js
@@ -18,22 +18,22 @@ export function randomList(length, min = -1.0, max = 1.0, dim = 2) {
   return list
 }
 
-export function nonLinearInterpellationGenerator(list, period) {
+export function nonLinearInterpellationGenerator(list, speed) {
   const len = list.length
-  const p = len / (period || len)
+  const p = speed
   const dim = Array.isArray(list[0]) ? list[0].length : 1
   return (t) => {
-    const i = mod(t * p, len)
+    const i = t * p
     const f = Math.floor(i)
     const c = Math.ceil(i)
     if (f === c) return list[i]
     else {
-      const v1 = list[f]
-      const v2 = list[c]
-      if (dim === 1) return v1 * (i - f) + v2 * (c - i)
+      const v1 = list[mod(f, len)]
+      const v2 = list[mod(c, len)]
+      if (dim === 1) return v1 * (c - i) + v2 * (i - f)
       const vec = []
       for ( let k = 0; k < dim; k++) {
-        vec.push(v1[k] * (i - f) + v2[k] * (c - i))
+        vec.push(v1[k] * (c - i) + v2[k] * (i - f))
       }
       return vec
     }
